c75813f6e8b0acd49b17c3badca29187b30cdf6a
takumac <takumi.sakai.engineer@gmail.com>
2022-10-15 00:50:41 +0900

    Initial Commit

---
+++ Mitemiru/MitemiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  MitemiruApp.swift
+//  Mitemiru
+//
+//  Created by Takumi Sakai on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct MitemiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
